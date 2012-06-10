69226f944f701e456f7a12431126034908f1b00b
Lukasz Dobrzanski <lukasz.m.dobrzanski@gmail.com>
2012-06-10 21:52:03 +0100

    bash completion portabilities
    - adding semicolons
    - fix for ```tr: warning: an unescaped backslash at end of string is not portable``` warnings

--- smsgates/extras/smsgates_bootstrap.sh
+++ smsgates/extras/smsgates_bootstrap.sh
@@ -1,14 +1,14 @@
-alias sms='sendsms.py -g "$SMSGATES_SENDSMS_GATE" -l "$SMSGATES_SENDSMS_LOGIN" -p "$SMSGATES_SENDSMS_PASSWORD" -t $*'
+alias sms='sendsms.py -g "$SMSGATES_SENDSMS_GATE" -l "$SMSGATES_SENDSMS_LOGIN" -p "$SMSGATES_SENDSMS_PASSWORD" -t $*';
 
 function _sendsms_complete {
-    local IFS=$'\n'
-    local cur="${COMP_WORDS[COMP_CWORD]}"
+    local IFS=$'\n';
+    local cur="${COMP_WORDS[COMP_CWORD]}";
     if [ -z "$cur" ]; then
-        COMPREPLY=( $(sendsms.py -s|awk -F, '{print $1; print $2}'|sort -u|sed -e 's/ /\\ /') )
+        COMPREPLY=( $(sendsms.py -s|awk -F, '{print $1; print $2}'|sort -u|sed -e 's/ /\\ /') );
     else
-        cur=`echo -n "$cur"|tr -d '\'`
-        COMPREPLY=( $(sendsms.py -s|awk -F, '{print $1; print $2}'|sort -u|grep -i "${cur}"|sed -e 's/ /\\ /') )
+        cur=$(echo -n "$cur"|tr -d '\\');
+        COMPREPLY=( $(sendsms.py -s|awk -F, '{print $1; print $2}'|sort -u|grep -i "${cur}"|sed -e 's/ /\\ /') );
     fi
 }
 
-complete -o default -F _sendsms_complete sms
+complete -o default -F _sendsms_complete sms;
